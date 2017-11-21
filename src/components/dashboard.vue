16076da4fedd6f9d914b371190593e4fc067c90d
JonasSchade <Jonas.Schade@stud.fh-rosenheim.de>
2017-11-21 19:08:56 +0100

    Further changes

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="dashboard">
+    <div class="hours">
+      <div class="hoursOverview">
+        <div class="row">
+          <p class="label">Stunden Soll:</p>
+          <p class="value">160</p>
+        </div>
+        <div class="row">
+          <p class="label">Stunden Ist:</p>
+          <p class="value">30</p>
+        </div>
+        <div class="row">
+          <p class="label">Differenz:</p>
+          <p class="value">130</p>
+        </div>
+      </div>
+      <div class="hoursOverview">
+        <div class="row">
+          <p class="label">Stunden Soll:</p>
+          <p class="value">160</p>
+        </div>
+        <div class="row">
+          <p class="label">Stunden Ist:</p>
+          <p class="value">30</p>
+        </div>
+        <div class="row">
+          <p class="label">Differenz:</p>
+          <p class="value">130</p>
+        </div>
+      </div>
+    </div>
     <div id='calendar'></div>
   </div>
 </template>
@@ -24,4 +54,23 @@ $(document).ready(function() {
     margin: 30px;
   }
 
+  .hoursOverview {
+    border: solid black 1px;
+    margin: 10px;
+    max-width: 300px;
+    width: 30%;
+    text-align: left;
+  }
+
+  .row {
+    margin: 10px;
+  }
+
+  .label, .value {
+    display: inline-block;
+    margin: 0px;
+  }
+  .value {
+    float: right;
+  }
 </style>
