75e79e7d25768c2a6cbd1d005fcdc926f6577945
JonasSchade <Jonas.Schade@stud.fh-rosenheim.de>
2017-11-22 22:52:18 +0100

    Hours now data, added calendar controls & little tweaks

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -12,15 +12,15 @@
               <div class="container-flex">
                 <div class="row">
                   <div class="col-sm-6"><span class="timelabel">Stunden Soll:</span></div>
-                  <div class="col-sm-6"><span class="timevalue">160 h</span></div>
+                  <div class="col-sm-6"><span class="timevalue">{{monthTime[0]}} h</span></div>
                 </div>
                 <div class="row">
                   <div class="col-sm-6"><span class="timelabel">Stunden Ist:</span></div>
-                  <div class="col-sm-6"><span class="timevalue">128,7 h</span></div>
+                  <div class="col-sm-6"><span class="timevalue">{{monthTime[1]}} h</span></div>
                 </div>
                 <div class="row">
                   <div class="col-sm-6"><span class="timelabel">Differenz:</span></div>
-                  <div class="col-sm-6"><span class="timevalue">31,3 h</span></div>
+                  <div class="col-sm-6"><span class="timevalue">{{Math.round ((monthTime[0] - monthTime[1])*10)/10}} h</span></div>
                 </div>
               </div>
             </div>
@@ -36,15 +36,15 @@
               <div class="container-flex">
                 <div class="row">
                   <div class="col-sm-6"><span class="timelabel">Soll:</span></div>
-                  <div class="col-sm-6"><span class="timevalue">1634,6 h</span></div>
+                  <div class="col-sm-6"><span class="timevalue">{{flexTime[0]}} h</span></div>
                 </div>
                 <div class="row">
                   <div class="col-sm-6"><span class="timelabel">Ist:</span></div>
-                  <div class="col-sm-6"><span class="timevalue">1283,4 h</span></div>
+                  <div class="col-sm-6"><span class="timevalue">{{flexTime[1]}} h</span></div>
                 </div>
                 <div class="row">
                   <div class="col-sm-6"><span class="timelabel">Differenz:</span></div>
-                  <div class="col-sm-6"><span class="timevalue">351,2</span></div>
+                  <div class="col-sm-6"><span class="timevalue">{{Math.round ((flexTime[0] - flexTime[1])*10)/10}} h</span></div>
                 </div>
               </div>
             </div>
@@ -52,7 +52,10 @@
         </div>
         <div class="col-sm-4">
           <div class="calendar-controls">
-            CALENDAR <br/>CONTROLS
+            <button v-on:click="calendarButtonPress('prev')"><i class="fa fa-arrow-left" aria-hidden="true"></i></button>
+            <button id="calendar-btn-today" v-on:click="calendarButtonPress('today')">Heute</button>
+            <button v-on:click="calendarButtonPress('next')"><i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+            <h4>{{calendarCurrentMonth}}</h4>
           </div>
         </div>
       </div>
@@ -71,27 +74,49 @@ export default {
   created: function () {
     //init calendar
     $(document).ready(function() {
-        $('#calendar').fullCalendar({
-          locale: 'de',
-          header: {
-              left:   '',
-              center: '',
-              right:  ''
-          }
-        })
+      $('#calendar').fullCalendar({
+        locale: 'de',
+        header: false,
+      });
+      $("#calendar-btn-today").click();
     });
+  },
+  data: function () {
+    return {
+      monthTime: [160,128.7],
+      flexTime: [1634.6,1283.4],
+      calendarCurrentMonth: 'error_init',
+    };
+  },
+  methods: {
+    calendarButtonPress: function(action) {
+      $('#calendar').fullCalendar(action);
+      this.updateCurrentMonth();
+    },
+    updateCurrentMonth: function() {
+      if (typeof $("#calendar").fullCalendar("getDate").startOf !== 'undefined') {
+        this.calendarCurrentMonth = $("#calendar").fullCalendar("getDate").startOf("month").format('MMMM YYYY');
+      } else {
+        this.calendarCurrentMonth = "error";
+      }
+    }
   },
 }
 </script>
 
 <style scoped>
 
-  i {
+  h4>i {
     padding-right: 7px;
   }
 
-  .dashboard>.col-sm-4 {
+  .dashboard .col-sm-4 {
     padding: 1%;
+  }
+
+  .dashboard>.container>.row {
+    margin-top: 5px;
+    margin-bottom: 5px;
   }
 
   .card {
@@ -149,6 +174,11 @@ export default {
   }
   .timevalue  {
     padding-right: 10px;
+  }
+
+  .fc-header-toolbar {
+    height: 0px !important;
+    margin: 0px !important;
   }
 
 </style>
