0b22485ef941a3866828549efec3601619fe0de6
JonasSchade <Jonas.Schade@stud.fh-rosenheim.de>
2017-11-26 00:00:50 +0100

    Added first inputs to dashboard-day-detail

--- src/components/dashboard/dashboard-day-detail.vue
+++ src/components/dashboard/dashboard-day-detail.vue
@@ -1,19 +1,50 @@
 <template>
   <div class="dashboard-day-detail">
     <div class="container">
-      <div class="row heading">
-        <div class="col-sm-3">
+      <div class="row">
+        <div class="col-sm-4  text-left">
           <button v-on:click="$router.push('/dashboard/')">
             <i class="fa fa-chevron-left" aria-hidden="true"></i>
             Dashboard
           </button>
         </div>
-        <div class="col-sm-6">
-          <h3>{{ formatedDate }}</h3>
+      </div>
+      <div class="row">
+        <div class="col-sm-12">
+          <h3>{{ formattedDate }}</h3>
+        </div>
+        <div class="col-sm-12 text-left">
+          <div class="card">
+            <h4 class="header">
+              <i class="fa fa-clock-o" aria-hidden="true"></i>Arbeitszeiten:
+            </h4>
+            <hr/>
+            <div class="content">
+              <form class="container">
+                <div class="row">
+                  <label class="col-sm-1 text-left">Von:</label>
+                  <div class="col-sm-3 text-left">
+                  <input type="text" id="time-start-hours" placeholder="hh" maxlength="2" size="2">
+                  <span>:</span>
+                  <input type="text" id="time-start-minutes" placeholder="mm" maxlength="2" size="2">
+                  <span>Uhr</span>
+                  </div>
+                </div>
+                <div class="row">
+                  <label class="col-sm-1 text-left">Bis:</label>
+                  <div class="col-sm-3 text-left">
+                    <input type="text" id="time-start-hours" placeholder="hh" maxlength="2" size="2">
+                    <span>:</span>
+                    <input type="text" id="time-start-minutes" placeholder="mm" maxlength="2" size="2">
+                    <span>Uhr</span>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-
   </div>
 </template>
 
@@ -22,7 +53,7 @@ export default {
   name: 'dashboard-day-detail',
 
   computed: {
-    "formatedDate": function () {
+    "formattedDate": function () {
       return moment(this.$route.params.day).locale("de").format("DD.MMMM YYYY");
     },
   },
@@ -30,10 +61,45 @@ export default {
 </script>
 
 <style scoped>
-  .dashboard-day-detail {
-    height: 700px;
-    text-align: center;
+
+  *[class^="col-sm-"] {
+    padding: 5px;
   }
+
+  .card {
+    background-color: white;
+    border-radius: 2px;
+    box-shadow: 0px 0px 7px #ccc;
+    border: 1px solid #ccc;
+    max-width: 400px;
+  }
+
+  .card>.header {
+    text-align: left;
+    border-top-left-radius: 2px;
+    border-top-right-radius: 2px;
+    color: #282828;
+    background-color: white;
+    margin-top: 0px;
+    margin-bottom: 0px;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    padding-left: 10px;
+  }
+
+  .card>.header>i {
+    padding-right: 7px;
+  }
+
+  .card>hr {
+    margin: 0px;
+  }
+
+  .card>.content {
+    padding-top: 5px;
+    padding-bottom: 5px;
+  }
+
 
   .row {
     margin-top: 5px;
@@ -42,34 +108,38 @@ export default {
     margin-right: 0px;
   }
 
-  .col-sm-6, .col-sm-3 {
+  div[class^="col-sm-"] {
     padding: 5px;
   }
 
   button {
-    float: left;
-  }
-
-  .heading button {
     border: 1px solid #ccc;
     box-shadow: 1px 1px 4px #ccc;
     color: #282828;
     background-color: white;
     padding: 5px 15px 5px 15px;
+    outline-width: 0;
   }
 
-  .heading button:hover {
+  button:hover {
     background-color: rgb(245,245,245);
   }
 
-  .heading button:disabled {
-    box-shadow: none;
-    border: 1px solid #dcdcdc;
-    background-color: #dcdcdc;
-    color: #a4a4a4;
+  h3 {
+    margin: 0px;
   }
 
-  .heading h3 {
+  input {
+    color: black;
     margin: 0px;
+    text-align: center;
+    border: none;
+    border-bottom: lightgrey 1px solid;
+    outline-width: 0;
+    transition: ease-in-out border-bottom .2s;
+  }
+
+  input:focus {
+    border-bottom: #003452 1px solid;
   }
 </style>
