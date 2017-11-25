fc99d75bb99a6ae312fb544433c434a1816307f6
JonasSchade <Jonas.Schade@stud.fh-rosenheim.de>
2017-11-25 21:52:36 +0100

    Added day-detail page

--- src/components/dashboard/dashboard-day-detail.vue
+++ src/components/dashboard/dashboard-day-detail.vue
@@ -1,12 +1,31 @@
 <template>
   <div class="dashboard-day-detail">
-    <h1>Day: {{ $route.params.day }}</h1>
+    <div class="container">
+      <div class="row heading">
+        <div class="col-sm-3">
+          <button v-on:click="$router.push('/dashboard/')">
+            <i class="fa fa-chevron-left" aria-hidden="true"></i>
+            Dashboard
+          </button>
+        </div>
+        <div class="col-sm-6">
+          <h3>{{ formatedDate }}</h3>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'dashboard-day-detail',
+
+  computed: {
+    "formatedDate": function () {
+      return moment(this.$route.params.day).locale("de").format("DD.MMMM YYYY");
+    },
+  },
 }
 </script>
 
@@ -16,8 +35,41 @@ export default {
     text-align: center;
   }
 
-  h1 {
-    display: block;
-    line-height: 700px;
+  .row {
+    margin-top: 5px;
+    margin-bottom: 5px;
+    margin-left: 0px;
+    margin-right: 0px;
+  }
+
+  .col-sm-6, .col-sm-3 {
+    padding: 5px;
+  }
+
+  button {
+    float: left;
+  }
+
+  .heading button {
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 4px #ccc;
+    color: #282828;
+    background-color: white;
+    padding: 5px 15px 5px 15px;
+  }
+
+  .heading button:hover {
+    background-color: rgb(245,245,245);
+  }
+
+  .heading button:disabled {
+    box-shadow: none;
+    border: 1px solid #dcdcdc;
+    background-color: #dcdcdc;
+    color: #a4a4a4;
+  }
+
+  .heading h3 {
+    margin: 0px;
   }
 </style>
