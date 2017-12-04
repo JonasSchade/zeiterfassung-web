c51ed427ad8738c8eb0ef367cbada2e1f4ed0a9f
JonasSchade <Jonas.Schade@stud.fh-rosenheim.de>
2017-12-04 11:51:47 +0100

    Further changes to dashboard day detail

--- src/components/dashboard/dashboard-day-detail.vue
+++ src/components/dashboard/dashboard-day-detail.vue
@@ -20,23 +20,53 @@
             </h4>
             <hr/>
             <div class="content">
-              <form class="container">
+              <form class="container-flex">
                 <div class="row">
-                  <label class="col-sm-1 text-left">Von:</label>
-                  <div class="col-sm-3 text-left">
-                  <input type="text" id="time-start-hours" placeholder="hh" maxlength="2" size="2">
+                  <label class="col-xs-2 text-left">Von:</label>
+                  <div class="col-xs-10 text-left">
+                  <input type="text" id="time-start-hours" @input="checkInput" placeholder="hh" maxlength="2" size="2">
                   <span>:</span>
-                  <input type="text" id="time-start-minutes" placeholder="mm" maxlength="2" size="2">
+                  <input type="text" id="time-start-minutes" @input="checkInput" placeholder="mm" maxlength="2" size="2">
                   <span>Uhr</span>
                   </div>
                 </div>
                 <div class="row">
-                  <label class="col-sm-1 text-left">Bis:</label>
-                  <div class="col-sm-3 text-left">
-                    <input type="text" id="time-start-hours" placeholder="hh" maxlength="2" size="2">
+                  <label class="col-xs-2 text-left">Bis:</label>
+                  <div class="col-xs-10 text-left">
+                    <input type="text" id="time-stop-hours" @input="checkInput" placeholder="hh" maxlength="2" size="2">
                     <span>:</span>
-                    <input type="text" id="time-start-minutes" placeholder="mm" maxlength="2" size="2">
+                    <input type="text" id="time-stop-minutes" @input="checkInput" placeholder="mm" maxlength="2" size="2">
                     <span>Uhr</span>
+                  </div>
+                </div>
+                <div class="row">
+                  <hr style="margin: 3px 10px;"/>
+                </div>
+                <div class="row">
+                  <label class="col-xs-2 text-left">Pause:</label>
+                  <div class="col-xs-10 text-left">
+                    <input type="text" id="time-break-hours" @input="checkInput" placeholder="hh" maxlength="2" size="2">
+                    <span>:</span>
+                    <input type="text" id="time-break-minutes" @input="checkInput" placeholder="mm" maxlength="2" size="2">
+                    <span>h</span>
+                  </div>
+                </div>
+                <div class="row">
+                  <label class="col-xs-2 text-left">Reise:</label>
+                  <div class="col-xs-10 text-left">
+                    <input type="text" id="time-travel-hours" @input="checkInput" placeholder="hh" maxlength="2" size="2">
+                    <span>:</span>
+                    <input type="text" id="time-travel-minutes" @input="checkInput" placeholder="mm" maxlength="2" size="2">
+                    <span>h</span>
+                  </div>
+                </div>
+                <div class="row">
+                  <hr style="margin: 3px 10px;"/>
+                </div>
+                <div class="row">
+                  <label class="col-xs-4 col-xs-offset-1 text-left">Insgesamt:</label>
+                  <div class="col-xs-4 text-left">
+                    <span>{{computedTime.hours}}:{{computedTime.minutes}} h</span>
                   </div>
                 </div>
               </form>
@@ -51,18 +81,90 @@
 <script>
 export default {
   name: 'dashboard-day-detail',
-
+  data: function (){
+    return {
+      computedTime: {
+        minutes: "00",
+        hours: "00",
+      },
+    };
+  },
   computed: {
     "formattedDate": function () {
       return moment(this.$route.params.day).locale("de").format("DD.MMMM YYYY");
     },
+  },
+  methods: {
+    checkInput: function (event) {
+      var elem = event.target;
+      var text = elem.value;
+
+      if (text.match("[^0-9]") !== null) {
+        elem.style.color = "red";
+        return;
+      }
+
+      var num = parseInt(text);
+      if (num > 23 || num < 0) {
+        elem.style.color = "red";
+        return;
+      }
+
+      //input validated
+      elem.style.color = "";
+/*
+      //check if every input field has valid input
+      for (var id in {"time-start-hours","time-start-minutes","time-stop-hours","time-stop-minutes","time-break-hours","time-break-minutes","time-travel-hours","time-travel-minutes"}) {
+        console.log(id);
+        var dom = document.getElementById(id);
+        if (dom.style.color == "red") {
+          return;
+        }
+      }
+*/
+
+      this.updateComputedTime();
+    },
+    updateComputedTime: function () {
+
+      var readValue = function (id) {
+        if (document.getElementById(id).value == "") {
+          return 0;
+        } else {
+          return parseInt(document.getElementById(id).value);
+        }
+      };
+
+      var moment1 = moment("2000-01-01");
+
+      //set stop time
+      moment1.hours(readValue("time-stop-hours"));
+      moment1.minutes(readValue("time-stop-minutes"));
+
+      //subtract start time
+      moment1.subtract(readValue("time-start-hours"),'h')
+      moment1.subtract(readValue("time-start-minutes"),'m')
+
+      //substract break
+      moment1.subtract(readValue("time-break-hours"),'h')
+      moment1.subtract(readValue("time-break-minutes"),'m')
+
+      //add travel time (half of it)
+      moment1.add(readValue("time-travel-hours")/2,'h')
+      moment1.add(readValue("time-travel-minutes")/2,'m')
+
+
+      this.computedTime.minutes = moment1.format("mm");
+      this.computedTime.hours = moment1.format("hh");
+
+    }
   },
 }
 </script>
 
 <style scoped>
 
-  *[class^="col-sm-"] {
+  *[class^="col-sm-"], *[class^="col-xs-"] {
     padding: 5px;
   }
 
@@ -108,10 +210,6 @@ export default {
     margin-right: 0px;
   }
 
-  div[class^="col-sm-"] {
-    padding: 5px;
-  }
-
   button {
     border: 1px solid #ccc;
     box-shadow: 1px 1px 4px #ccc;
@@ -141,5 +239,9 @@ export default {
 
   input:focus {
     border-bottom: #003452 1px solid;
+  }
+
+  label{
+    padding-left: 15px !important;
   }
 </style>
