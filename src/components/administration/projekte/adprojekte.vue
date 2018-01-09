227af8ddaa68357ec7cffcc2a6dc0e55ea9c8280
TimmWerner <timm.werner@schmerreim.de>
2018-01-09 15:51:45 +0100

    adprojekte schoen gemacht und auf dynamischen inhalt angepasst

--- src/components/administration/projekte/adprojekte.vue
+++ src/components/administration/projekte/adprojekte.vue
@@ -1,96 +1,152 @@
 <template>
-  <div class="adprojekte">
-    <router-link to="/newProject">
-      <button id="btn_new_project"><i class="fa fa-plus" aria-hidden="true"></i> Neues Projekt</button>
-    </router-link>
-    <div class="container">
-      <div class="header">
-          <h1>Projekte</h1>
-      </div>
+<div class="adprojekte">
+  <div class="container">
 
-        <div role="tablist">
-          <div v-for="project in projects">
-            <b-card no-body class="mb-1">
-              <b-card-header header-tag="header" class="p-1" role="tab">
-                <b-btn class="pjkt_button" block href="#" v-b-toggle.accordion variant="info">Name: {{project.Name}}</b-btn>
-              </b-card-header>
-              <b-collapse id="accordion1" visible accordion="my-accordion" role="tabpanel">
-                <b-card-body>
+    <div class="topper">
+      <div class="row">
+        <div class="col-sm-4 text-left">
+          <button v-on:click="$router.push('/administration/')">
+        <i class="fa fa-chevron-left" aria-hidden="true"></i>
+        Administration
+      </button>
+        </div>
+        <div class="col-sm-4  text-middle">
+          <h3>Projekte</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-4">
+        </div>
+        <div class="col-sm-4">
+        </div>
+        <div class="col-sm-4 text-right">
+          <router-link to="/newProject">
+            <button id="btn_new_project"><i class="fa fa-plus" aria-hidden="true"></i> Neues Projekt</button>
+          </router-link>
+        </div>
+      </div>
+    </div>
+
+    <div role="tablist">
+      <div v-for="project in projects">
+        <b-card class="mb-1">
+          <b-card-header header-tag="header" class="p-1" role="tab">
+            <div class="row">
+              <div class="col-sm-12">
+                <div class="pjkt_button" block v-b-toggle="'test-'+project.ID">{{project.Name}}
+                  <i class="settings fa fa-pencil-square-o " v-on:click="$router.push('/administration/')" aria-hidden="true"></i>
+                </div>
+              </div>
+
+            </div>
+          </b-card-header>
+          <b-card-body>
+            <b-collapse v-bind:id="'test-'+project.ID" visible accordion="my-accordion" role="tabpanel">
+
+              <div class="information">
+
                   <p class="card-text">
-                      {{ project.Manager }}
+                    {{ project.Manager }}
                   </p>
+
                   <p class="card-text">
                     {{ text }}
                   </p>
-                </b-card-body>
-              </b-collapse>
-            </b-card>
-          </div>
 
+              </div>
 
-        </div>
+            </b-collapse>
+          </b-card-body>
+        </b-card>
+      </div>
+
 
     </div>
+
   </div>
+</div>
 </template>
 
 <script>
 export default {
   name: 'adprojekte',
-  data () {
-   return {
-     projects: []
-   }
- },
-   created() {
+  data() {
+    return {
+      projects: []
+    }
+  },
+  created() {
 
     Vue.http.get('http://localhost:3000/api/projekte').then(response => {
-        console.log(response);
-        this.projects = response.body;
+      console.log(response);
+      this.projects = response.body;
     });
   }
 }
 </script>
 
 <style scoped>
-  .adprojekte {
-    height: 700px;
-    text-align: center;
-  }
-.header{
+.adprojekte {
+  height: 700px;
+  text-align: center;
+}
+
+.header {
   background-color: #cdd2d8;
   padding: 30px;
   padding-top: 10px;
   padding-bottom: 10px;
   margin: 0px;
 }
-  h1 {
-    display: block;
-    margin: 20px;
-  }
-  #btn_new_project{
-    box-shadow: 1px 1px 4px #ccc;
-    color: #282828;
-    background-color: white;
-    padding: 10px;
-    margin: 25px;
-    border: 1px solid #ccc;
-  }
-  .container{
-    position: relative;
-    max-width: 800px;
-    top: 20px;
-    bottom: 20px;
-    box-shadow: 0 3px 5px -1px rgba(0, 0, 0, .2), 0 6px 10px 0 rgba(0, 0, 0, .14), 0 1px 18px 0 rgba(0, 0, 0, .12);
-    margin-bottom: 30px;
-    padding-left: 0px;
-    padding-right: 0px;
-  }
-  .mb-1{
-    margin: 10px;
-  }
-  .pjkt_button {
-      background-color: #003452;
-      padding: 10px;
-  }
+
+h1 {
+  display: block;
+  margin: 20px;
+}
+
+.container {
+  position: relative;
+  max-width: 800px;
+  top: 20px;
+  bottom: 20px;
+  margin-bottom: 30px;
+  padding-left: 0px;
+  padding-right: 0px;
+}
+
+.mb-1 {
+  margin-bottom: 30px;
+}
+
+.pjkt_button {
+  text-align: left;
+  background-color: #003452;
+  padding: 20px;
+  border: none;
+  font-size: 18px;
+  color: white;
+  box-shadow: 0px 0px 7px #ccc;
+}
+
+.topper {
+  margin-bottom: 30px;
+}
+
+.settings {
+  color: white;
+  background-color: none;
+  float: right;
+  line-height: 26px;
+}
+
+.information{
+  border-radius: 0px 0px 3px 3px;
+  box-shadow: 0px 0px 7px #ccc;
+  padding: 20px;
+}
+
+.p-1{
+    box-shadow: 0px 0px 7px #ccc;
+}
+
 </style>
