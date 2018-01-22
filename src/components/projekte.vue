9615b838183727c1701890f89091fe8579f4e611
TimmWerner <timm.werner@schmerreim.de>
2018-01-22 17:41:52 +0100

    Projekte seite fertig und läuft

--- src/components/projekte.vue
+++ src/components/projekte.vue
@@ -9,15 +9,11 @@
             <div class="col-sm-6">
               <h4>Manager:</h4>
               <p class="card-text">{{project.firstname}} {{project.lastname}}</p>
-            </div>
-            <div class="col-sm-6">
+              <hr />
               <h4>Beschreibung:</h4>
               <p class="card-text">{{ project.description }}</p>
             </div>
-          </div>
-            <hr/>
-          <div class="row">
-            <div class="col-xs-4">
+            <div class="col-sm-6">
               <h4>Zugewiesene Mitarbeiter:</h4>
               <ul class="userlist">
                 <li class="row" v-for="user in getUsers(project.id)">
@@ -26,10 +22,21 @@
                 </li>
               </ul>
             </div>
-            <div class="col-xs-8">
+          </div>
+          <hr/>
+          <div class="row">
+            <div>
               <div>
-                <LineExample :labels="labels" :datasets="datasets">
-                </LineExample>
+                <table id="arbeitszeiten">
+                  <tr>
+                    <th>Mitarbeiter</th>
+                    <th v-for="day in labels">{{day}}</th>
+                  </tr>
+                  <tr v-for="user in getUsers(project.id)">
+                    <td>{{user.firstname}} {{user.lastname}}</td>
+                    <td v-for="time in getTimeforUser(project.id, user.id)">{{time.dur}} Std.</td>
+                  </tr>
+                </table>
               </div>
             </div>
           </div>
@@ -48,7 +55,10 @@ import tablistitem from '@/components/administration/tablistitem'
 import LineExample from '@/components/LineChart.js'
 export default {
   name: 'projekte',
-  components: {tablistitem, LineExample},
+  components: {
+    tablistitem,
+    LineExample
+  },
   data() {
     return {
       benutzer: "",
@@ -57,8 +67,8 @@ export default {
       projects: [],
       clickeduser: "",
       time_array: [],
-      labels:['Januar', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets:[{
+      labels: ['Januar', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [{
         label: 'Data One',
         borderColor: '#FC2525',
         data: [40, 39, 10, 40, 39, 80, 40]
@@ -66,76 +76,143 @@ export default {
     }
   },
   methods: {
-    getUsers: function(id){
+    getUsers: function(id) {
       return this.users[id.toString()];
     },
-    //WICHTIG! Wird im Moment nur aufgerufen bei Projekt "Datenbank" bei KLick auf Timm Werner
-    fillChartwithData: function(projectid, userid){
-      var options = { year: 'numeric', month: 'long', day: 'numeric' };
+    getTimeforUser: function(projectid, userid) {
+      console.log("zgzjjgetTimeforUser: ");
+      return this.time_array[projectid.toString()][userid.toString()];
+    },
+    // //WICHTIG! Wird im Moment nur aufgerufen bei Projekt "Datenbank" bei KLick auf Timm Werner
+    // fillChartwithData: function(projectid, userid) {
+    //   var options = {
+    //     year: 'numeric',
+    //     month: 'numeric',
+    //     day: 'numeric'
+    //   };
+    //   var today = new Date();
+    //   for (var j = 6; j >= 0; j--) {
+    //     if (j == 6) {
+    //       this.labels[6] = today.toLocaleString('de-DE', options);
+    //     } else {
+    //       today.setDate(today.getDate() - 1);
+    //       this.labels[j] = today.toLocaleString('de-DE', options);
+    //     }
+    //
+    //     //parse today into the dateformat the api reqires
+    //     var month = ("0" + (today.getMonth() + 1)).slice(-2); // month (in integer 0-11)
+    //     var year = today.getFullYear();
+    //     var day = today.getUTCDate();
+    //     var todayISO = year + '-' + month + '-' + day
+    //
+    //     this.$http.get('http://localhost:3000/api/project_time/' + userid + '/' + todayISO + '/' + projectid, {
+    //       headers: {
+    //         Authorization: ('bearer ' + window.sessionStorage.chronosAuthToken)
+    //       }
+    //     }).then(response => {
+    //       this.time_array.push(response.body[0].duration);
+    //     }).then(response => {
+    //       if (j <= 0) {
+    //         var dataset = {
+    //           label: userid,
+    //           borderColor: 'green',
+    //           data: this.time_array
+    //         };
+    //         this.datasets.push(dataset);
+    //       }
+    //     });
+    //   }
+    //   console.log("Labels: " + this.labels)
+    //
+    //   //  this.addData($('#time_chart'),this.labels, time_array);
+    //
+    //
+    //
+    // },
+    clickuser: function(projectid, userid) {
+      var array = this.users[projectid];
+      for (var i = 0; i < array.length; i++) {
+        if (array[i].id == userid) {
+          this.clickeduser = array[i];
+        }
+      }
+      //this.fillChartwithData(projectid, userid);
+    },
+    fillChartwithData2: function(projectid) {
+      var options = {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric'
+      };
       var today = new Date();
-      this.labels[6] = today.toLocaleString('de-DE', options);
-      for(var j = 5; j>=0; j--){
-        today.setDate(today.getDate() - 1);
-        this.labels[j] = today.toLocaleString('de-DE', options);
+      for (var j = 6; j >= 0; j--) {
+        if (j == 6) {
+          this.labels[6] = today.toLocaleString('de-DE', options);
+        } else {
+          today.setDate(today.getDate() - 1);
+          this.labels[j] = today.toLocaleString('de-DE', options);
+        }
 
         //parse today into the dateformat the api reqires
         var month = ("0" + (today.getMonth() + 1)).slice(-2); // month (in integer 0-11)
         var year = today.getFullYear();
         var day = today.getUTCDate();
-        var todayISO = year+'-'+month+'-'+day
+        var todayISO = year + '-' + month + '-' + day
 
-        this.$http.get('http://localhost:3000/api/project_time/'+userid+'/'+todayISO+'/'+projectid, {headers: {Authorization: ('bearer '+ window.sessionStorage.chronosAuthToken)}}).then(response => {
-          this.time_array[0] = response.body[0].duration;
-          console.log(response.body[0].duration);
-          console.log(response.body[0].name);
+        console.log("projektid: "+projectid);
+        console.log("user des projects: "+this.getUsers(projectid));
+        this.getUsers(projectid).forEach((e) => {
+          this.$http.get('http://localhost:3000/api/project_time/' + e.id + '/' + todayISO + '/' + projectid, {
+            headers: {
+              Authorization: ('bearer ' + window.sessionStorage.chronosAuthToken)
+            }
+          }).then(response => {
+            console.log(e.userid+"Gearbeitet: "+response.body[0]);
+            this.time_array[projectid][e.id].push({dur: response.body[0].duration, name: response.body[0].userid });
+          });
         });
       }
-      console.log("Labels: "+this.labels)
-      console.log("Time_array: "+this.time_array)
-      var dataset = {
-        label: userid,
-        borderColor: 'green',
-        data: this.time_array
-      }
-      this.datasets.push(dataset);
-    //  this.addData($('#time_chart'),this.labels, time_array);
-      console.log("datasets: "+this.datasets)
-
-
-    },
-    clickuser: function(projectid, userid){
-      var array = this.users[projectid];
-      for (var i = 0; i<array.length; i++){
-        if(array[i].id==userid){
-          this.clickeduser = array[i];
-        }
-      }
-      this.fillChartwithData(projectid, userid);
-    },
-    // addData: function(chart, label, data) {
-    // chart.data.labels.push(label);
-    // chart.data.datasets.forEach((dataset) => {
-    //     dataset.data.push(data);
-    // });
-    // chart.update();
-    // }
+    }
   },
   created() {
-    this.$http.get('http://localhost:3000/api/authenticate', {headers: {Authorization: ('bearer '+ window.sessionStorage.chronosAuthToken)}}).then(response => {
+    this.$http.get('http://localhost:3000/api/authenticate', {
+      headers: {
+        Authorization: ('bearer ' + window.sessionStorage.chronosAuthToken)
+      }
+    }).then(response => {
       this.benutzer = response.body.username;
       this.userId = response.body.id;
 
-      this.$http.get('http://localhost:3000/api/projects_of_manager/'+this.userId, {headers: {Authorization: ('bearer '+ window.sessionStorage.chronosAuthToken)}}).then(response => {
+      this.$http.get('http://localhost:3000/api/projects_of_manager/' + this.userId, {
+        headers: {
+          Authorization: ('bearer ' + window.sessionStorage.chronosAuthToken)
+        }
+      }).then(response => {
         this.projects = response.body;
         for (var i = 0; i < this.projects.length; i++) {
-          this.$http.get('http://localhost:3000/api/project_users/'+this.projects[i].id, {headers: {Authorization: ('bearer '+ window.sessionStorage.chronosAuthToken)}}).then(response => {
-            var id = response.url.replace("http://localhost:3000/api/project_users/","");
+          console.log("Nochmal Projectid: "+this.projects[i].id);
+          this.$http.get('http://localhost:3000/api/project_users/' + this.projects[i].id, {
+            headers: {
+              Authorization: ('bearer ' + window.sessionStorage.chronosAuthToken)
+            }
+          }).then(response => {
+            var id = response.url.replace("http://localhost:3000/api/project_users/", "");
             this.users[id.toString()] = response.body;
+            console.log("Response body: "+response.body);
+            for(var j = 0; j<response.body.length; j++){
+              if (this.time_array[id] == null) {
+                this.time_array[id] = [];
+              }
+              this.time_array[id][response.body[j].id] = [];
+            }
+              console.log(this.time_array);
+
+            this.fillChartwithData2(id);
+            console.log("Nach fill: "+this.time_array)
           });
         }
       });
     });
-    //this.fillChartwithData();
   }
 }
 </script>
@@ -185,5 +262,26 @@ export default {
   padding: 0px;
   margin: 0px;
   margin-bottom: 10px;
+}
+
+#arbeitszeiten {
+  width: 100%;
+}
+
+tr {
+  background-color: #f2f4f7;
+  padding: 10px;
+  border-bottom: 1px solid white;
+}
+
+td {
+  padding: 10px;
+}
+
+th {
+  background-color: #003452;
+  color: white;
+  border-radius: 3px 3px 0px 0px;
+  padding: 10px;
 }
 </style>
