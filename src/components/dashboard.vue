e686d57ad8d40fec3b28b23081c1e77fc7bba3a9
TimmWerner <timm.werner@schmerreim.de>
2018-01-24 14:27:38 +0100

    aktueller-Monat-Anzeige im Dashboard läääuft

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -189,13 +189,16 @@ export default {
         component.calendarUpdateControls();
 
       });
+      this.setTimeReqirements();
     });
+
   },
   data() {
     return {
       monthTime: [160,128.7],
-      yearTime: [1000,700],
+      yearTime: [1700,700],
       userid: Number,
+      token: 0
     };
   },
   methods: {
@@ -212,7 +215,6 @@ export default {
       dateString = dateString.concat("-01");
 
       $('#calendar').fullCalendar('gotoDate', dateString);
-
       this.calendarUpdateControls();
     },
     //updates buttons (disable/enable) and selects based on current date of calendar
@@ -229,6 +231,28 @@ export default {
       $("#calendar-btn-next")[0].disabled = ($("#calendar-select-month")[0].selectedIndex == $("#calendar-select-month")[0].length -1 && $("#calendar-select-year")[0].selectedIndex == $("#calendar-select-year")[0].length -1);
       $("#calendar-btn-today")[0].disabled = $('#calendar').find('td.fc-today').length !== 0;
 
+      console.log("Test")
+      this.setTimeReqirements(month, year);
+    },
+    setTimeReqirements(month, year) {
+      var DaysOfCurrentMonth = moment(month).daysInMonth();
+      var workedTime = 0.0;
+      var firstDay = moment(year+""+ month +"01");
+      var lastDay = moment(year+""+ month +""+ DaysOfCurrentMonth).add(1, 'days');
+
+      for (var m = moment(firstDay); m.isBefore(lastDay); m.add(1, 'days')) {
+        console.log(m.format('YYYY-MM-DD'));
+        this.$http.get('http://localhost:3000/api/time_by_user_date/'+this.userid+"/"+m.format('YYYY-MM-DD'), {headers: {Authorization: ('bearer '+ window.sessionStorage.chronosAuthToken)}}).then(response => {
+          //console.log(response.body.sum);
+          workedTime = workedTime + response.body.sum;
+          this.token = this.token + 1;
+          console.log("token:"+this.token);
+          console.log(workedTime);
+          if(this.token==DaysOfCurrentMonth){
+            this.monthTime= [DaysOfCurrentMonth*8, workedTime];
+          }
+        });
+      }
     }
   },
 }
